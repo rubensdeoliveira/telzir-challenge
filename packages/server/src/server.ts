b1e71e72032e256aef26771f64776f315d4a3448
rubensojunior <rubensojunior6@gmail.com>
2020-09-08 21:18:05 -0300

    adding routes to server

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 
+import routes from './routes'
+
 const app = express()
 
 app.use(cors())
-
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('Server stated on port 3333')
